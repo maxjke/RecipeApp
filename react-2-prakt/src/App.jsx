0b5b3654814ec6599f26261e03153b0b7da21320
maxjke <m.jako030@gmail.com>
2025-03-25 21:17:19 +0000

    fix

--- react-2-prakt/src/App.jsx
+++ react-2-prakt/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
+import { AuthProvider, AuthContext } from './components/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import Home from './Pages/Home';
@@ -15,32 +15,47 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <nav style={{ marginBottom: '20px' }}>
-          <Link to="/">Home</Link> |{' '}
-          <Link to="/login">Login</Link> |{' '}
-          <Link to="/register">Register</Link> |{' '}
-          <Link to="/dashboard">Dashboard</Link> |{' '}
-          <Link to="/recipes">Recipes</Link> |{' '}
-          <Link to="/favorites">Favorites</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/recipes" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
+        <AppContent />
       </Router>
     </AuthProvider>
+  );
+}
+
+function AppContent() {
+  const { auth } = useContext(AuthContext);
+
+  return (
+    <>
+      <nav style={{ marginBottom: '20px' }}>
+      
+        {!auth.isAuthenticated && <Link to="/">Home</Link>}
+        {!auth.isAuthenticated && <> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link> | </>}
+        {auth.isAuthenticated && (
+          <>
+            <Link to="/dashboard">Dashboard</Link> |{' '}
+            <Link to="/recipes">Recipes</Link> |{' '}
+            <Link to="/favorites">Favorites</Link>
+          </>
+        )}
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/recipes" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </>
   );
 }
 
